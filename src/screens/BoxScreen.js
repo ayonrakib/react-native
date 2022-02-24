8df90469bbfe75bb93964f80185850b8ad947b88
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-02-24 00:08:20 -0600

    box model

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,20 +5,53 @@ export default function BoxScreen(){
 
     const styles = StyleSheet.create({
         viewStyle : {
-            backgroundColor: "blue",
-            borderWidth: 3
-        },
-        textStyle : {
             borderWidth: 3,
-            backgroundColor: "gray",
-            margin: 10
+            borderColor: 'black',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+        },
+        textOneStyle : {
+            borderWidth: 3,
+            borderColor : 'red',
+            height: 50,
+            width: 50,
+            backgroundColor: 'gray'
+        },
+        textTwoStyle : {
+            borderWidth: 3,
+            borderColor : 'red',
+            alignSelf: 'center',
+            height: 50,
+            width: 50,
+            backgroundColor: 'yellow'
+        }
+        ,
+        textThreeStyle : {
+            borderWidth: 3,
+            borderColor : 'red',
+            height: 50,
+            width: 50,
+            backgroundColor: 'green'
         }
     })
     return (
-        <View style = {styles.viewStyle}>
-            <Text style = {styles.textStyle}>
-                Box screen Method!
-            </Text>
+        <View>
+            <View style = {styles.viewStyle}>
+                <Text style = {styles.textOneStyle}>
+                    Child 1
+                </Text>
+
+                <Text style = {styles.textThreeStyle}>
+                    Child 3
+                </Text>
+            </View>
+            <View>
+                <Text style = {styles.textTwoStyle}>
+                    Child 2
+                </Text>
+            </View>
         </View>
+
+
     )
 }
